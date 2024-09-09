d4dbaaf0482c9b2d0e33cab364f5e42911dc3fbc
Clayton Smith <ccsmith33@crimson.ua.edu>
2024-09-09 17:36:30 +0100

    final

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿double tip, sand, top, discount, total;
+int numSand, numTop;
+
+discount = 0.9;
+sand = 4.75;
+top = 0.55;
+Console.Clear();
+Console.WriteLine("How Many Sandwiches Would you like?");
+
+numSand = int.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("How Many Toppings Would you like?");
+
+numTop = int.Parse(Console.ReadLine());
+
+total = (numSand * sand + numTop * top) * discount;
+Console.Clear();
+Console.Write("Your Subtotal is $");
+total = Math.Round(total, 2);
+Console.Write(total);
+Console.WriteLine(". Please enter your tip, expressed as a decimal.");
+
+
+tip = double.Parse(Console.ReadLine());
+
+total = total + total *tip;
+
+Console.Clear();
+Console.Write("Your Total is $");
+
+total = Math.Round(total, 2);
+Console.WriteLine(total);
+
